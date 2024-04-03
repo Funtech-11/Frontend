fe1912154b20401d804dffdc49c903dd02fd023a
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-04-03 23:09:25 +0300

    husky test

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,7 +1,11 @@
 import style from './MainPage.module.scss';
 
 const MainPage = () => {
-  return <div className={style.main}>MainPage</div>;
+  return (
+    <div className={style.main}>
+      <div>MainPage</div>
+    </div>
+  );
 };
 
 export default MainPage;
