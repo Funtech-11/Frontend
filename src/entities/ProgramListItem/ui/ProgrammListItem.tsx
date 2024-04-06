be94aa97afce007263a2d5b1e7167f8f13c73de3
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-04-06 15:53:04 +0300

    feat: add programm markup

--- src/entities/ProgramListItem/ui/ProgrammListItem.tsx
+++ src/entities/ProgramListItem/ui/ProgrammListItem.tsx
@@ -1,8 +1,51 @@
+import type { FC } from 'react';
+
+import { ContentLinkBtn } from 'src/features/ContentLinkBtn';
+import type { TProgram } from 'src/widgets/ProgrammList/types/type';
+
 import style from './ProgrammListItem.module.scss';
 
-const ProgrammListItem = () => {
-  console.log('hello linter');
-  return <div className={style.container}>ListItem</div>;
+type TProgramItemProps = {
+  programItem: TProgram;
+};
+
+const formatTime = (timeString: string) => {
+  // Извлекаем часы и минуты из строки времени
+  const [hours, minutes] = timeString.slice(11, 16).split(':');
+  // Возвращаем отформатированную строку времени
+  return `${hours}:${minutes}`;
+};
+
+const ProgrammListItem: FC<TProgramItemProps> = ({ programItem }) => {
+  console.log(programItem);
+  return (
+    <div className={style.container}>
+      <div className={style.time}>{formatTime(programItem.time)}</div>
+      <div className={style.infoWrapper}>
+        <div className={style.title}>{programItem.name}</div>
+        <div className={style.infoText}>{programItem.information}</div>
+        {programItem.speaker && (
+          <div className={style.speakerInfoWrapper}>
+            <div className={style.avatarBox}>
+              <img
+                src={programItem.speaker[0].avatar}
+                alt="avatar"
+                className={style.avatarImg}
+              />
+            </div>
+            <div className={style.speakerInfoContainer}>
+              <h4 className={style.name}>{programItem.speaker[0].name}</h4>
+              <p className={style.job}>{programItem.speaker[0].job}</p>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className={style.btnWrapper}>
+        {programItem.materials && <ContentLinkBtn type="button" />}
+        {programItem.video && <ContentLinkBtn type="link" />}
+      </div>
+    </div>
+  );
 };
 
 export default ProgrammListItem;
--- src/widgets/ProgrammList/ui/ProgrammList.tsx
+++ src/widgets/ProgrammList/ui/ProgrammList.tsx
@@ -1,13 +1,17 @@
 import { ProgrammListItem } from 'src/entities/ProgramListItem';
 import style from './ProgrammList.module.scss';
 
+import { event } from '../const/mockData';
+
 const ProgrammList = () => {
   console.log('hello linter');
   return (
     <section className={style.container}>
       <h2 className={style.title}>Программа</h2>
       <div>
-        <ProgrammListItem />
+        {event.programs.map(programItem => (
+          <ProgrammListItem key={programItem.id} programItem={programItem} />
+        ))}
       </div>
     </section>
   );
