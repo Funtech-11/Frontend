b242713af6c4ea35f5fc039512bd32dd2f289320
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-04-06 23:14:08 +0300

    feat: add filledCardForm

--- src/widgets/FilledRegistrationForm/ui/FilledRegistrationForm.tsx
+++ src/widgets/FilledRegistrationForm/ui/FilledRegistrationForm.tsx
@@ -1,3 +1,4 @@
+import avatarIcon from 'src/assets/images/avatars/4b20ef67ab74659b2552d4e86c81968d.jpeg';
 import style from './FilledRegistrationForm.module.scss';
 
 const FilledRegistrationForm = () => {
@@ -8,8 +9,42 @@ const FilledRegistrationForm = () => {
       <p className={style.subtitle}>
         С этой формой Вы можете регистрироватьсяна любое мероприятие в один клик
       </p>
+      <div className={style.card}>
+        <div className={style.containerr}>
+          <div className={style.userInfoContainer}>
+            <div className={style.avatarWrapper}>
+              <img className={style.avatar} src={avatarIcon} alt="аватар" />
+            </div>
+            <div className={style.infoWrapper}>
+              <p className={style.name}>Александр Васильев</p>
+              <p className={style.job}>Тимлид в ЗАО «КРОК»</p>
+            </div>
+          </div>
+          <div className={style.infoGrid}>
+            <div className={style.textBlock}>
+              <span className={style.text}>Телефон:</span>
+              <span className={style.text}>+7 (985) 123 - 77 - 77</span>
+            </div>
+            <div className={style.textBlock}>
+              <span className={style.text}>Почта:</span>
+              <span className={style.text}>Alex_Vasiliev@yandex.ru</span>
+            </div>
+            <div className={style.textBlock}>
+              <span className={style.text}>Направление:</span>
+              <span className={style.text}>Backend</span>
+            </div>
+            <div className={style.textBlock}>
+              <span className={style.text}>Стек:</span>
+              <span className={style.text}>Ruby</span>
+            </div>
+          </div>
+        </div>
+        <button type="button">Редактировать</button>
+      </div>
     </section>
   );
 };
 
 export default FilledRegistrationForm;
+
+// TODO кнопка редактировать общая оранжевая
