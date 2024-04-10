4e7b501bb508d587c67f990fb53235555852cb0b
Elizaveta Ananeva-Ryashchenko <elizaveta.elenovna@yandex.ru>
2024-04-10 08:29:41 +0300

    feat: create button component, install svgr

---
+++ src/entities/Button/ui/Button.tsx
@@ -0,0 +1,47 @@
+import { FC, MouseEvent } from 'react';
+import { useNavigate } from 'react-router-dom';
+import ForwardIcon from 'src/assets/images/components/forward.svg';
+import style from './Button.module.scss';
+
+interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  title: string;
+  path?: string;
+  variant?: string; // primary, outlined, text
+  hasIcon?: boolean;
+  extraClass?: string;
+}
+
+const Button: FC<IButtonProps> = ({
+  title,
+  path,
+  variant = 'primary',
+  hasIcon = false,
+  extraClass,
+  ...ButtonHTMLAttributes
+}) => {
+  const navigate = useNavigate();
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    if (path) {
+      navigate(path);
+      window.scrollTo(0, 0);
+    }
+  };
+
+  return (
+    <button
+      className={`${style.button} ${style[variant]} ${extraClass}`}
+      onClick={handleClick}
+      {...ButtonHTMLAttributes}
+    >
+      <span>{title}</span>
+      {hasIcon && (
+        <div className={style.icon}>
+          <ForwardIcon />
+        </div>
+      )}
+    </button>
+  );
+};
+
+export default Button;
--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,10 +1,13 @@
-import { Footer } from 'src/widgets/Footer';
+import { useState } from 'react';
 import { Header } from 'src/widgets/Header';
+// import { Menu } from 'src/widgets/Menu';
+import { Chips } from 'src/widgets/Chips';
+import { Card } from 'src/widgets/Card';
+import { Button } from 'src/entities/Button';
+import { Footer } from 'src/widgets/Footer';
 import style from './MainPage.module.scss';
 
 import { mockThemes, mockCards } from 'src/utils/mocks';
-import Chips from 'src/widgets/Chips/ui/Chips';
-import { Card } from 'src/widgets/Card';
 
 const MainPage = () => {
   console.log('hello linter');
@@ -24,6 +27,9 @@ const MainPage = () => {
             );
           })}
         </ul>
+        <div className={style.contentContainer}>
+          <Button title="Ещё" hasIcon={true} />
+        </div>
       </div>
       <Footer />
     </div>
