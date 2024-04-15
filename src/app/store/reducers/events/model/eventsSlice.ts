76e92bbc13a695fc78697b9c9567f681f71bb947
NatashaSolntseva <84683371+NatashaSolntseva@users.noreply.github.com>
2024-04-15 21:09:49 +0300

    feat: reducers added post get delete events lication speaker

--- src/app/store/reducers/events/model/eventsSlice.ts
+++ src/app/store/reducers/events/model/eventsSlice.ts
@@ -1,7 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { IEvent } from 'src/shared/api/events/dtos';
 import { initialEvent } from './constants';
-import { getEventById, getEventsCards } from 'src/shared/api/events';
+import {
+  createEvent,
+  deleteEvent,
+  getEventById,
+  getEventsCards,
+  updateEvent,
+} from 'src/shared/api/events';
 
 interface IEventsState {
   events: IEvent[];
@@ -35,6 +41,7 @@ const eventsSlice = createSlice({
         state.error = action.payload;
         state.isLoading = false;
       })
+
       .addCase(getEventById.pending, state => {
         state.isLoading = true;
       })
@@ -44,6 +51,53 @@ const eventsSlice = createSlice({
         state.error = null;
       })
       .addCase(getEventById.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(deleteEvent.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(deleteEvent.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.events = state.events.filter(
+          event => event.eventId !== action.payload
+        );
+        state.error = null;
+      })
+      .addCase(deleteEvent.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(createEvent.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(createEvent.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.events.push(action.payload);
+        state.error = null;
+      })
+      .addCase(createEvent.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      })
+      .addCase(updateEvent.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(updateEvent.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const updatedEvent = action.payload;
+        const index = state.events.findIndex(
+          event => event.eventId === updatedEvent.eventId
+        );
+        if (index !== -1) {
+          state.events[index] = updatedEvent;
+        }
+        state.error = null;
+      })
+      .addCase(updateEvent.rejected, (state, action) => {
         state.error = action.payload;
         state.isLoading = false;
       });
--- src/app/store/reducers/locations/model/locationsSlice.ts
+++ src/app/store/reducers/locations/model/locationsSlice.ts
@@ -1,7 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { initialLocation } from './constants';
 import { ILocation } from 'src/shared/api/locations/dtos';
-import { getLocationById, getLocations } from 'src/shared/api/locations';
+import {
+  createLocation,
+  deleteLocation,
+  getLocationById,
+  getLocations,
+  updateLocation,
+} from 'src/shared/api/locations';
 
 interface ILocationState {
   locations: ILocation[];
@@ -35,6 +41,7 @@ const locationsSlice = createSlice({
         state.error = action.payload;
         state.isLoading = false;
       })
+
       .addCase(getLocationById.pending, state => {
         state.isLoading = true;
       })
@@ -46,11 +53,61 @@ const locationsSlice = createSlice({
       .addCase(getLocationById.rejected, (state, action) => {
         state.error = action.payload;
         state.isLoading = false;
+      })
+
+      .addCase(deleteLocation.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(deleteLocation.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.locations = state.locations.filter(
+          location => location.locationId !== action.payload
+        );
+        state.error = null;
+      })
+      .addCase(deleteLocation.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      })
+
+      .addCase(createLocation.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(createLocation.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.locations.push(action.payload);
+        state.error = null;
+      })
+      .addCase(createLocation.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
+      })
+
+      .addCase(updateLocation.pending, state => {
+        state.isLoading = true;
+        state.error = null;
+      })
+      .addCase(updateLocation.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const updatedLocation = action.payload;
+        const index = state.locations.findIndex(
+          location => location.locationId === updatedLocation.locationId
+        );
+        if (index !== -1) {
+          state.locations[index] = updatedLocation;
+        }
+        state.error = null;
+      })
+      .addCase(updateLocation.rejected, (state, action) => {
+        state.error = action.payload;
+        state.isLoading = false;
       });
   },
 });
 
-export const selectEvents = (state: { locations: ILocationState }) =>
+export const selectLocations = (state: { locations: ILocationState }) =>
   state.locations;
 
 export default locationsSlice.reducer;
